fb97d03bf7235c325ce5f91cc78509493fdf7e2b
SeCardenas <se.cardenas@uniandes.edu.co>
2021-02-03 15:54:46 -0800

    fix production

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,2 @@
 if __name__ == '__main__':
-	if len(sys.argv) > 1 and sys.argv[1] == 'init':
-		db.create_all()
-		email = os.environ['ADMINEMAIL']
-		password = os.environ['ADMINPASSWORD']
-		if db.session.query(User).filter_by(email=email).count() < 1:
-			db.session.add(User(
-				email=email,
-				password=guard.hash_password(password),
-				roles='admin'
-				))
 	app.run()
